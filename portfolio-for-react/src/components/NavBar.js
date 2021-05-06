6caaa06159d687b585ab70d274814dfa39a9db19
Hanna Ferris <hferris@dbq.edu>
2021-05-05 20:08:41 -0700

    Working on navbar

---
+++ portfolio-for-react/src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar() {
+  const location = useLocation();
+  return (
+    <div>
+      <ul className="nav nav-bar">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={
+              location.pathname === "/" ? "nav-link active" : "nav-link"
+            }
+          >
+            HF
+          </Link>
+        </li>
+        <li className="nav-one">
+          <Link
+            to="/about"
+            className={
+              location.pathname === "/about" ? "nav-link active" : "nav-link"
+            }
+          >
+            About Me
+          </Link>
+        </li>
+        <li className="nav-two">
+          <Link
+            to="/work"
+            className={
+              location.pathname === "/work" ? "nav-link active" : "nav-link"
+            }
+          >
+            MY Work
+          </Link>
+        </li>
+        <li className="nav-three">
+          <Link
+            to="/contact"
+            className={
+              location.pathname === "/contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact Information
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
