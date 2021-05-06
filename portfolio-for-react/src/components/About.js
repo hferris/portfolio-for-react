72e12894af779e073f36fe9470acf983651303d2
Hanna Ferris <hferris@dbq.edu>
2021-05-05 23:14:51 -0700

    completed about me info

---
+++ portfolio-for-react/src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const styles = {
+  backgroundColor: "#E8632E ",
+  textAlign: "center",
+  fontFamily: "Roboto, Times New Roman, Times, serif",
+};
+
+function About() {
+  return (
+    <div style={styles}>
+      <h2>Get to know Hanna</h2>
+      <p>
+        Welcome to my digital portfolio, my name is Hanna Ferris. I graduated
+        from the University of Dubuque with a Bachelor of Business
+        Administration in Marketing and Communication Minor. I am currently
+        working to get a Web Development Certificate by taking on a full-stack
+        coding program, through Trilogy at the University of Arizona with an
+        expected graduation date of May 10, 2021. Upon graduation, I want to be
+        able to incorporate my marketing degree with the certificate to land a
+        career in the IT industry. My goal is to become a Business Analyst or
+        Quality Assurance Specialist in a company, where I’ll be able to utilize
+        my background in marketing and my talents in Web Development to
+        strengthen business requirements within IT.
+      </p>
+    </div>
+  );
+}
+
+export default About;
